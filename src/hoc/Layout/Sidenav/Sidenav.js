8d161777e0ecb81d2f0add0ec87695ee268c26c7
IsabelNzioka <isabelnzioka@gmail.com>
2022-08-14 21:40:33 +0300

    responsive

--- src/hoc/Layout/Sidenav/Sidenav.js
+++ src/hoc/Layout/Sidenav/Sidenav.js
@@ -2,30 +2,42 @@ import React from "react";
 
 import classes from "./Sidenav.module.css";
 import SideNavLink from "./SidenavLink/SidenavLink";
+import { HiMenu } from "react-icons/hi";
 
-const Navbar = () => (
-  <nav className={classes.nav}>
-    {/* <div className={classes.navbar}> */}
-    <div className={classes.NavLogo}>
-      <h4>
-        Bella's <span className="Logo"> Cakery.</span>
-      </h4>
-    </div>
+const Navbar = () => {
+  const handleClick = () => {
+    // console.log("clicked");
+    const ul = document.querySelector("ul");
+    ul.classList.toggle(classes.showMenu);
+  };
+  return (
+    <nav className={classes.nav}>
+      {/* <div className={classes.navbar}> */}
+      <div className={classes.NavLogo}>
+        <h3>
+          Bella's <span className={classes.Logo}> Cakery.</span>
+        </h3>
 
-    <ul className={classes.NavItems}>
-      <SideNavLink link="/">Cakes </SideNavLink>
-      <SideNavLink link="/cakes">catering </SideNavLink>
-      <SideNavLink link="/about">About Us </SideNavLink>
-      <SideNavLink link="/home">home </SideNavLink>
-      <SideNavLink link="/hours">Hours & Locations </SideNavLink>
-      <SideNavLink link="/giftcards">Gift cards </SideNavLink>
-    </ul>
+        <div onClick={handleClick} className={classes.Menu}>
+          <HiMenu />
+        </div>
+      </div>
 
-    <div className={classes.NavLocation}>
-      <button className={classes.btn}>Order</button>
-    </div>
-    {/* </div> */}
-  </nav>
-);
+      <ul className={classes.NavItems}>
+        <SideNavLink link="/">Cakes </SideNavLink>
+        <SideNavLink link="/cakes">catering </SideNavLink>
+        <SideNavLink link="/about">About Us </SideNavLink>
+        <SideNavLink link="/home">home </SideNavLink>
+        <SideNavLink link="/hours"> Locations </SideNavLink>
+        <SideNavLink link="/giftcards">Gift cards </SideNavLink>
+      </ul>
+
+      <div className={classes.NavLocation}>
+        <button className={classes.btn}>Order</button>
+      </div>
+      {/* </div> */}
+    </nav>
+  );
+};
 
 export default Navbar;
